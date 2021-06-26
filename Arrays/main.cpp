573db4ecca148ca0b2d67e77291586c1286f039e
Eugene Stolbin <estolbin@gmail.com>
2021-06-26 14:19:56 +0300

    Overload in progress

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -4,10 +4,15 @@ using namespace std;
 #define tab "\t"
 
 void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
 void Print(int arr[], const int n);
+void Print(double arr[], const int n);
 void PrintReverse(int arr[], const int n);
+void PrintReverse(double arr[], const int n);
 int Sum(int arr[], const int n);
+double Sum(double arr[], const int n);
 double Avg(int arr[], const int n);
+double Avg(double arr[], const int n);
 int minValueIn(int arr[], const int n);
 int maxValueIn(int arr[], const int n);
 
@@ -24,6 +29,13 @@ void main()
 	cout << "Среднее арифметическое: " << Avg(arr, n) << endl;
 	cout << "Минимальное значение в массиве: "<< tab << minValueIn(arr, n) << endl;
 	cout << "Максимальное значение в массиве: " << tab << maxValueIn(arr, n) << endl;
+
+	double d_arr[n];
+	FillRand(d_arr, n);
+	Print(d_arr, n);
+	PrintReverse(d_arr, n);
+	cout << "Сумма элементов массива (double): " << Sum(d_arr, n) << endl;
+	cout << "Среднее арифметическое (double): " << Avg(d_arr, n) << endl;
 }
 
 void FillRand(int arr[], const int n)
@@ -31,6 +43,13 @@ void FillRand(int arr[], const int n)
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100;
+	}
+}
+void FillRand(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = double(rand() % 100) / 10;
 	}
 }
 
@@ -42,8 +61,24 @@ void Print(int arr[], const int n)
 	}
 	cout << endl;
 }
+void Print(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
 
 void PrintReverse(int arr[], const int n)
+{
+	for (int i = n-1; i >= 0; i--)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void PrintReverse(double arr[], const int n)
 {
 	for (int i = n-1; i >= 0; i--)
 	{
@@ -61,10 +96,23 @@ int Sum(int arr[], const int n)
 	}
 	return sum;
 }
+double Sum(double arr[], const int n)
+{
+	double sum = 0.0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
 
 double Avg(int arr[], const int n)
 {
 	return (double)Sum(arr, n) / n;
+}
+double Avg(double arr[], const int n)
+{
+	return Sum(arr, n) / n;
 }
 
 int minValueIn(int arr[], const int n) 
